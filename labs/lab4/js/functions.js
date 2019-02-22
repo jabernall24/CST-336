d64f9b7be88b76cde5568e833219b06cc2f238d8
Jesus Andres Bernal Lopez <jebernal@csumb.edu>
2019-02-22 01:51:40 +0000

    worked on lab 4

--- labs/lab4/js/functions.js
+++ labs/lab4/js/functions.js
@@ -5,7 +5,7 @@ $("#zipCode").on("change", function(){
         url: "http://itcdland.csumb.edu/~milara/ajax/cityInfoByZip.php",
         dataType: "json",
         data: {
-            "zip": $(this).val(),
+            "zip": $("#zipCode").val(),
         },
         success: function(data,status) {
             $("#city").html(data.city);
@@ -13,8 +13,65 @@ $("#zipCode").on("change", function(){
             $("#longitude").html(data.longitude);
         },
         complete: function(data,status) { //optional, used for debugging purposes
-            // alert(status);
+            if(status == "error"){
+                $("#noZipCode").html("zip code not found");
+            }
         }
 
     });//ajax
 }); //zipCode
+
+$("#submitBtn").on("click", function(){
+    if ($("#userName").val().length < 4){
+        $("#nameError").html("Incorrect username");
+    }
+    if($("#password").val().length < 6){
+        $("#passwordError").html("Password needs to be at least 6 characters.");
+    }
+    if($("#confirmPassword").val() != $("#password").val()){
+        $("#passwordError").html("Retype password");
+    }
+});
+//ipoilliot@csumb.edu  ilona poilliot
+$("#userName").on("change", function() {
+    $.ajax({
+        type: "GET",
+        url: "http://myspace.csumb.edu/~milara/ajax/username/usernameLookup.php",
+        dataType: "json",
+        data: {
+            "username": $("#userName").val(),
+        },
+        success: function(data,status) {
+            if (data.available == "true"){
+                $("#nameError").html("Valid Name").css("color", "green");
+            }else{
+                $("#nameError").html("Invalid Name").css("color", "red");
+            }
+        },
+        complete: function(data,status) { //optional, used for debugging purposes
+            // alert(status);
+        }
+
+    });//ajax
+});
+
+$("#state").on("change", function(){
+    $.ajax({
+        type: "GET",
+        url: "http://itcdland.csumb.edu/~milara/ajax/countyList.php",
+        dataType: "json",
+        data: {
+            "state": $("#state").val(),
+        },
+        success: function(data,status) {
+            $("#county").html("<option> Select One </option>");
+            for(let i = 0; i < data.length; i++){
+                $("#county").append("<option>" + data[i].county + "</option>");
+            }
+        },
+        complete: function(data,status) { //optional, used for debugging purposes
+            // alert(status);
+        }
+
+    });//ajax
+}); // state
