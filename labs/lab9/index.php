c26ce45d95199d5e9dec5d8e4fd0a61cfec688bb
Jesus Andres Bernal Lopez <jebernal@csumb.edu>
2019-05-05 03:45:59 +0000

    finished lab 9

--- labs/lab9/index.php
+++ labs/lab9/index.php
@@ -1,20 +1,45 @@
 <?php
-    print_r($_FILES);
+     
+    function displayImagesUploaded() {
+        if(!empty($_FILES) && $_FILES["fileName"]["size"] <= 1000000) {
+            move_uploaded_file($_FILES["fileName"]["tmp_name"], "gallery/" . $_FILES["fileName"]["name"]);
+        }
+     
+        $images = scandir("gallery"); // returns all file names within that folder
+        
+        for($i = 2; $i < count($images); $i++) {
+            
+            echo "<a href='gallery/$images[$i]'  target='_blank'><img src='gallery/$images[$i]' width='100'/><a>";
+        }
+    }
+
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
         <title> Lab 9: File upload </title>
+        
+        <!--bootstrap-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        
+        <link rel="stylesheet" href="css/index.css" type="text/css" />
+
     </head>
     <body>
         
-        <h1> File Upload </h1>
+        <div class='jumbotron'>
+            <h1> File Upload </h1>
+        </div>
         
         <form method="POST" enctype="multipart/form-data"> 
             Select file: <input type="file" name="fileName" /> <br />
             <button>Upload File!</button>
         </form>
-
+        
+        <h3> Images Uploaded: </h3>
+        
+        <?=displayImagesUploaded() ?>
     </body>
 </html>
